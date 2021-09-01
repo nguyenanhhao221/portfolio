03994d3615b9f4ad45d07d6b5a4b9d6b16635919
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2021-09-02 02:16:17 +0700

    finish project section draft layout

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,17 @@
 const hamburger = document.getElementById("hamburger");
+const navUL = document.getElementById("nav-ul")
+const nav = document.getElementById("nav")
 
-const displayFunc = () => {
-    
-}
-hamburger.addEventListener('click',)
+const displayNav = () => {
+    if (navUL.style.visibility = "hidden") {
+        navUL.style.visibility = "visible"
+    } 
+    navUL.style.visibility = "hidden";
+};
+
+const removeNav = () => navUL.style.visibility = "hidden";
+
+hamburger.addEventListener('click', displayNav);
+// hamburger.addEventListener('mouseout',removeNav);
+
 
